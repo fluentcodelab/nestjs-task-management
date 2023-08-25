3bd7a4c3ceda9dd05582b764a94199366c417194
Gabin Dongmo <gdongmo@gmail.com>
2023-08-24 20:06:17 -0400

    Feature: Signing Up

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class AuthService {
   constructor(private repository: UsersRepository) {}
+  async signUp(dto: AuthCredentialsDto): Promise<void> {
+    return this.repository.createUser(dto);
+  }
 }
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,10 +1,17 @@
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { Injectable } from '@nestjs/common';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class UsersRepository extends Repository<User> {
   constructor(private dataSource: DataSource) {
     super(User, dataSource.createEntityManager());
+  }
+
+  async createUser(dto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = dto;
+    const user = this.create({ username, password });
+    await this.save(user);
   }
 }
