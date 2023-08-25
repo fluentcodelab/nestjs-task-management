3eee5a9156f1369d0279567f518b646fa4da737d
Gabin Dongmo <gdongmo@gmail.com>
2023-08-24 22:58:17 -0400

    Implementing Jwt validation

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,27 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UsersRepository } from './users.repository';
+import { JwtPayload } from './jwt-payload.interface';
+import { User } from './user.entity';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private repository: UsersRepository) {
+    super({
+      secretOrKey: 'ultraSecret145',
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<User> {
+    const { username } = payload;
+    const user = await this.repository.findOne({ where: { username } });
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
